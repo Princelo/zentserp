85ad4bf234f64a5cb97bd1e4fb3617ae934c562b
Princelo <lamkimcheung@gmail.com>
2015-05-26 11:33:49 +0800

    report user month

--- application/views/report/query_sub.php
+++ application/views/report/query_sub.php
@@ -46,12 +46,6 @@
                                 </td>
                             </tr>
                             <tr>
-                                <th>过滤无收支日期</th>
-                                <td>
-                                    <input type="checkbox" name="is_filter" />
-                                </td>
-                            </tr>
-                            <tr>
                                 <th>起始日期</th>
                                 <td><input name="date_from" class="datepicker" /></td>
                             </tr>
